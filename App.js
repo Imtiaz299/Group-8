178321f00cd110a335f79bb8a1c9802cc20c7c65
uchchwas1 <47281896+uchchwas1@users.noreply.github.com>
2019-02-19 15:19:25 -0800

    Create App.js
    by expo init bachelorAccommodation

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import DrawerNavigator from './navigation/DrawerNavigator'
+
+
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Home</Text>
+        <HomeScreen />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+  }
+  
+});
+
