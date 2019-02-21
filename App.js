45d0400379de4062a1575b911e99e2dfdbb6ba8d
uchchwas1 <47281896+uchchwas1@users.noreply.github.com>
2019-02-21 13:40:55 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
 import DrawerNavigator from './navigation/DrawerNavigator'
-
-
-
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Home</Text>
         <DrawerNavigator />
       </View>
     );
@@ -20,10 +17,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-
-  
 });
-
