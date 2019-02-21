aeed47386a53d6dc490b948b9db95384bad0b758
uchchwas1 <47281896+uchchwas1@users.noreply.github.com>
2019-02-21 13:21:40 -0800

    for customize menuDrawer

---
+++ components/MenuDrawer.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const WIDTH = Dimensions.get('window').width 
+const HEIGHT = Dimensions.get('window').height 
+
+export default class MenuDrawer extends React.Component {
+	navLink(nav, text) {
+		return(
+			<TouchableOpacity style={{height: 50}} onPress={() => this.props.navigation.navigate(nav)}>
+				<Text style={styles.link}>{text}</Text>
+			</TouchableOpacity>
+		)
+	}
+
+	render() {
+		return(
+			<View style={styles.container}>
+				
+					<View style={styles.topLinks}>
+						
+							
+								
+							</View>
+						
+					<View style={styles.bottomLinks}>
+						{this.navLink('Home', 'Home')}
+						{this.navLink('About', 'About')}
+						{this.navLink('Settings', 'Settings')}
+					</View>
+				
+			</View>
+		)
+	}
+
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: 'lightgray',
+	},
+	
+
+	topLinks:{
+		height: 160,
+		backgroundColor: 'black',
+	},
+	bottomLinks: {
+		flex: 1,
+		backgroundColor: 'white',
+		paddingTop: 10,
+		paddingBottom: 450,
+	},
+	link: {
+		flex: 1,
+		fontSize: 20,
+		padding: 6,
+		paddingLeft: 14,
+		margin: 5,
+		textAlign: 'left',
+	},
+	
+})
