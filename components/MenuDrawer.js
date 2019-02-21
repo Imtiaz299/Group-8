77db6e0a7bb3c0eb9d626db35c7cc3a39c816e16
uchchwas1 <47281896+uchchwas1@users.noreply.github.com>
2019-02-21 13:41:58 -0800

    add profie, name, update view

--- components/MenuDrawer.js
+++ components/MenuDrawer.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import {
+	View, 
+	Text,
+	Image,
+	ScrollView,
+	Platform,
+	Dimensions,
+	StyleSheet,
+	TouchableOpacity,
+} from 'react-native';
 
 const WIDTH = Dimensions.get('window').width 
 const HEIGHT = Dimensions.get('window').height 
@@ -15,31 +25,69 @@ export default class MenuDrawer extends React.Component {
 	render() {
 		return(
 			<View style={styles.container}>
-				
+				<ScrollView style={styles.scroller}>
 					<View style={styles.topLinks}>
-						
-							
-								
+						<View style={styles.profile}>
+							<View style={styles.imgView}>
+								<Image style={styles.img} source={require('../assets/uchchwas.jpg')} />
 							</View>
-						
+							<View style={styles.profileText}>
+								<Text style={styles.name}>Uchchwas Das</Text>
+							</View>
+						</View>
+					</View>
 					<View style={styles.bottomLinks}>
 						{this.navLink('Home', 'Home')}
 						{this.navLink('About', 'About')}
 						{this.navLink('Settings', 'Settings')}
 					</View>
-				
+				</ScrollView>
+				<View style={styles.footer}>
+					<Text style={styles.description}>Menu Tutorial</Text>
+					<Text style={styles.version}>v1.0</Text>
+				</View>
 			</View>
 		)
 	}
-
+}
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: 'lightgray',
 	},
-	
-
+	scroller: {
+		flex: 1,
+	},
+	profile: {
+		flex: 1,
+		flexDirection: 'row',
+		alignItems: 'center',
+		paddingTop: 25,
+		borderBottomWidth: 1,
+		borderBottomColor: '#777777',
+	},
+	profileText: {
+		flex: 3,
+		flexDirection: 'column',
+		justifyContent: 'center',
+	},
+	name: {
+		fontSize: 20,
+		paddingBottom: 5,
+		color: 'white',
+		textAlign: 'left',
+	},
+	imgView: {
+		flex: 1,
+		paddingLeft: 20,
+		paddingRight: 20,
+	},
+	img: {
+		height: 70,
+		width: 70,
+		borderRadius: 50,
+	},
 	topLinks:{
 		height: 160,
 		backgroundColor: 'black',
@@ -58,5 +106,23 @@ const styles = StyleSheet.create({
 		margin: 5,
 		textAlign: 'left',
 	},
-	
+	footer: {
+		height: 50,
+		flexDirection: 'row',
+		alignItems: 'center',
+		backgroundColor: 'white',
+		borderTopWidth: 1,
+		borderTopColor: 'lightgray'
+	},
+	version: {
+		flex: 1, 
+		textAlign: 'right',
+		marginRight: 20,
+		color: 'gray'
+	},
+	description: {
+		flex: 1, 
+		marginLeft: 20,
+		fontSize: 16,
+	}
 })
