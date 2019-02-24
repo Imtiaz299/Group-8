0d56022c846cae6a4ce7760af9f81786554c715c
uchchwas1 <47281896+uchchwas1@users.noreply.github.com>
2019-02-24 00:06:55 -0800

    import firebase

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import firebase from 'firebase';
 import DrawerNavigator from './navigation/DrawerNavigator'
+
+var config = {
+  apiKey: "AIzaSyAZNBDNtxdGC9dFnpNGlqKtAIAb2wvt7hY",
+  authDomain: "bacheloraccommodation-815cd.firebaseapp.com",
+  databaseURL: "https://bacheloraccommodation-815cd.firebaseio.com",
+  projectId: "bacheloraccommodation-815cd",
+  storageBucket: "bacheloraccommodation-815cd.appspot.com",
+  messagingSenderId: "290763445002"
+};
+
+firebase.initializeApp(config);
 
 export default class App extends React.Component {
   render() {
