0321eef8396ab70ff5207838e5d19639a50a2c3c
uchchwas1 <47281896+uchchwas1@users.noreply.github.com>
2019-03-23 21:36:25 -0800

    Extend code with props navigation, AuthStateChanged and modify frontend

--- Project-Code/screens/LoginScreen.js
+++ Project-Code/screens/LoginScreen.js
@@ -1,60 +1,94 @@
 import React from 'react';
-//import { ExpoLinksView } from '@expo/samples';
-import * as FirebaseAPI from '../modules/firebaseAPI'
-import { StyleSheet, Text, View, Platform, TextInput, TouchableOpacity
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  Platform,
+  TextInput,
+  TouchableOpacity,
  } from 'react-native';
+import firebase from 'firebase'
+import * as FirebaseAPI from '../modules/firebaseAPI';
 
 import MenuButton from '../components/MenuButton'
 
 export default class LoginScreen extends React.Component {
-  static navigationOption ={
-    title: 'Links',
+  constructor(props) {
+    super(props);
+  }
+  
+  static navigationOptions = {
+    title: 'Login',
   };
 
-  state= {
+  state = {
     email: "Enter email",
     password: "Enter password"
   };
 
-  submit(){
-       FirebaseAPI.createUser(this.state.email, this.state.password)
+  componentDidMount() {
+    this.watchAuthState(this.props.navigation)
   }
+
+  watchAuthState(navigation) {
+    firebase.auth().onAuthStateChanged(function(user) {
+      console.log('onAuthStatheChanged: ', user)
+      
+      if (user) {
+        navigation.navigate('Main');
+      }
+    });
+  }
+
+  createUser() {
+    FirebaseAPI.createUser(this.state.email, this.state.password)
+  }
+
+  signIn() {
+    FirebaseAPI.signInUser(this.state.email, this.state.password)
+  }
+
 
   render() {
     return (
-      
       <View style={styles.container}>
         <View style={styles.textContainer}>
           <Text style={styles.text}>Create an account below</Text>
-            <TextInput
-              style={styles.textInput}
-              onChangeText={(text)=>this.setState({email})}
-              value={this.state.email}
-            />
+          <TextInput 
+            style={styles.textInput}
+            onChangeText={(text) => this.setState({email: text})}
+            value={this.state.email}
+          />
+          <TextInput 
+            style={styles.textInput}
+            onChangeText={(text) => this.setState({password: text})}
+            value={this.state.password}
+          />
 
-            <TextInput
-              style={styles.textInput}
-              onChangeText={(text)=>this.setState({password})}
-              value={this.state.password}
-            />
+          <TouchableOpacity
+            style={{marginTop: '5%'}}
+            onPress={() => this.signIn()}
+          >
+            <View>
+              <Text>Log In Existing</Text>
+            </View>
+          </TouchableOpacity>
 
-            <TouchableOpacity
-              style={{marginTop: '5%'}}
-              onPress={() => {}}>
-              <View>
-                 <Text>Submit</Text>
-              </View>
-            </TouchableOpacity>
-            <MenuButton navigation={this.props.navigation} />
-          </View>
+
+          <TouchableOpacity
+            style={{marginTop: '5%'}}
+            onPress={() => this.createUser()}
+          >
+            <View>
+              <Text>Create New User</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
-         
-     
-      
     );
   }
-}
 
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
